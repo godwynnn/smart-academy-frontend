ea86523471c6f49ec99f3f0bc6b021d9dfb2abac
godwynnn <miraclegodwin67@gmail.com>
2025-07-20 00:40:49 +0100

    init

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -183,10 +183,10 @@ export default function Sidebar({ children }) {
                         </div>
 
                         <div className="p-4 border-t border-gray-200 dark:border-neutral-700">
-                            <Link onClick={Logout} className="flex justify-between items-center gap-x-3 py-2 px-3 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:hover:bg-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-300 dark:focus:text-neutral-300" href="#">
+                            <p onClick={Logout} className="flex justify-between items-center gap-x-3 py-2 px-3 text-sm text-gray-700 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:hover:bg-neutral-900 dark:text-neutral-400 dark:hover:text-neutral-300 dark:focus:text-neutral-300" >
                                 logout
                                 <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" /><polyline points="10 17 15 12 10 7" /><line x1="15" x2="3" y1="12" y2="12" /></svg>
-                            </Link>
+                            </p>
                         </div>
                     </div>
                     {/* <!-- End Footer --> */}
