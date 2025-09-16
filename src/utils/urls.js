3e52ad3a5dc4d4a582a62809f70f5e832ba96451
godwynnn <miraclegodwin67@gmail.com>
2025-09-16 14:54:43 +0100

    init

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,6 @@
 export const Urls=()=>{
-    const url='http://127.0.0.1:8000/'
-    // const url='https://smart-academy-ujq0.onrender.com/'
+    // const url='http://127.0.0.1:8000/'
+    const url='https://smart-academy-ujq0.onrender.com/'
 
     return{
         ws_url:'ws://127.0.0.1:8000/ws/chat/',
